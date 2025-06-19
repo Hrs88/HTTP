a42fbf3a45d3594de207244b36362ed86ba6284f
Hrs88 <hh153129741@163.com>
2025-06-20 02:15:09 +0800

    完善关闭脚本

--- rm.sh
+++ rm.sh
@@ -1,4 +1,4 @@
-ps ajx | grep serverd | grep -v grep | awk '{print $1}' | xargs sudo kill -9
+ps ajx | grep serverd | grep -v grep | awk '{print $2}' | xargs sudo kill -9
 cd cgi_wp/message/
 make clean
 cd ../../
