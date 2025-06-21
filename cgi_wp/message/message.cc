b6855fa44956fd3179e7c1d5d7b761c0f45acf80
Hrs88 <hh153129741@163.com>
2025-06-21 18:34:50 +0800

    表单提交信息存入mysql数据库

--- cgi_wp/message/message.cc
+++ cgi_wp/message/message.cc
@@ -3,10 +3,55 @@
 #include<string>
 #include<unordered_map>
 #include<unistd.h>
+#include<time.h>
 #include<strings.h>
+#include<mysql/mysql.h>
 #include"toolkit.hpp"
+//#define DEBUG
 static const size_t default_buff_size = 128;
 static const int read_fd = 0;
+static const std::string default_file = "logs/message.txt";
+namespace mysql
+{
+    static const std::string default_host = "localhost";
+    static const std::string default_user = "holly";
+    static const std::string default_password = "linktome";
+    static const std::string default_database = "httpserver";
+    static const unsigned int default_port = 3306;
+}
+std::string get_time()
+{
+    char buff[default_buff_size] = {0};
+    time_t t = time(nullptr);
+    struct tm* timeinfo = localtime(&t);
+    strftime(buff,sizeof(buff),"%Y-%m-%d %H:%M:%S",timeinfo);
+    return std::string(buff);
+}
+void to_file(std::unordered_map<std::string,std::string>& kv)
+{
+    std::string time = get_time();
+    std::fstream file(default_file.c_str(),std::ios::app);
+    file << "time: " << time << std::endl;
+    file << "name: " << UrlCoder::UrlDecode(kv["name"]) << std::endl;
+    file << "contact: " << UrlCoder::UrlDecode(kv["contact"]) << std::endl;
+    file << "body: " << UrlCoder::UrlDecode(kv["body"]) << std::endl;
+    file << "----------Divider----------" << std::endl;
+}
+void to_mysql(std::unordered_map<std::string,std::string>& kv)
+{
+    using namespace mysql;
+    MYSQL* link = mysql_init(nullptr);
+    mysql_real_connect(link,default_host.c_str(),default_user.c_str(),default_password.c_str(),default_database.c_str(),default_port,nullptr,0);
+    mysql_set_character_set(link,"utf-8");
+    std::string query = "insert message (time,name,contact,msg) values (\"" + get_time() + "\",\"" + 
+    UrlCoder::UrlDecode(kv["name"]) + "\",\"" + UrlCoder::UrlDecode(kv["contact"]) + "\",\"" + UrlCoder::UrlDecode(kv["body"]) + "\");";
+#ifdef DEBUG
+    std::fstream file(default_file.c_str(),std::ios::app);
+    file << query << std::endl;
+#endif
+    mysql_query(link,query.c_str());
+    mysql_close(link);
+}
 int main()
 {
     std::string method = getenv("METHOD");
@@ -26,10 +71,7 @@ int main()
     }
     std::unordered_map<std::string,std::string> kv;
     deal_post(body,kv);
-    std::fstream file("logs/message.txt",std::ios::app);
-    file << "name: " << UrlCoder::UrlDecode(kv["name"]) << std::endl;
-    file << "contact: " << UrlCoder::UrlDecode(kv["contact"]) << std::endl;
-    file << "body: " << UrlCoder::UrlDecode(kv["body"]) << std::endl;
-    file << "----------Divider----------" << std::endl;
+    //to_file(kv);
+    to_mysql(kv);
     return 0;
 }
